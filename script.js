c0224cbdab744f42e39459176c9458abe54ac230
TrestoXD <vitogamer08@gmail.com>
2025-12-12 17:12:53 +0100

    Cosa.com

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function Menu_displace(){
     if(document.getElementById("menuaside").style.opacity == 0){
         console.log("INSISIBLE")
         document.getElementById("menuaside").style.opacity = 1;
+        document.getElementById("menuaside").style.visibility = 1;
     }else{
         console.log("VISIBLE")
         document.getElementById("menuaside").style.opacity = 0;
+        document.getElementById("menuaside").style.visibility = 0;
     }
 }
