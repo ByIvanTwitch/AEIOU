72bf184cac86f938dff1b59ed80dad9972a7fef2
TrestoXD <vitogamer08@gmail.com>
2025-12-16 18:36:34 +0100

    cosas

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
+    let aside = true;
+    let loged = true;
+
+    console.log(aside)
+    Menu_displace()
+    Menu_profile()
+
 function Menu_displace(){
     console.log(document.getElementById("menuaside").classList)
 
 
-    if(document.getElementById("menuaside").style.opacity == 0){
+    if(aside == false){
         console.log("INSISIBLE")
         document.getElementById("menuaside").style.opacity = 1;
         document.getElementById("menuaside").style.visibility = 1;
+        document.getElementById("menuaside").style.transform = "translate(0 ,0)";
+        aside = true
     }else{
         console.log("VISIBLE")
         document.getElementById("menuaside").style.opacity = 0;
         document.getElementById("menuaside").style.visibility = 0;
+        document.getElementById("menuaside").style.transform = "translate(-10vw ,10px)";
+        aside = false
     }
 }
 
 function Menu_profile(){
-    if(document.getElementById("loged").style.opacity == 0){
+    if(loged == false){
         document.getElementById("loged").style.opacity = 1;
+        document.getElementById("loged").style.transform = "translate(-89vw ,-65px);";
+        loged = true
     }
     else{
         document.getElementById("loged").style.opacity = 0;
+        document.getElementById("loged").style.transform = "translate(89vw ,65px)";
+        loged = false
     }
 }
